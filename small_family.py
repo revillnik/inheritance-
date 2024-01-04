35be63e3b8407efb38bee58d6163ecbd78075352
revillnik <revillnik@gmail.com>
2024-01-04 20:36:59 +0300

    yse

---
+++ small_family.py
@@ -0,0 +1,25 @@
+class Animals:
+    def give_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        print(self.name)
+
+
+class Cats(Animals):
+    def __init__(self):
+        print(
+            'Вы завели кошку, как хотите её назвать? (впишите имя через команду x.give_name("имя")'
+        )
+
+
+class Dogs(Animals):
+    def __init__(self):
+        print(
+            'Вы завели собаку, как хотите её назвать? Впишите имя через команду x.give_name("имя")'
+        )
+
+
+vas = Cats()
+vas.give_name("Пуська")
+vas.get_name()
