db970a16b820819624248e2f127f71ce303f5be6
revillnik <revillnik@gmail.com>
2024-01-05 18:26:06 +0300

    next

--- geom.py
+++ geom.py
@@ -1,10 +1,13 @@
 class Geom:
+    name = 'Geom'
     def __init__(self, x, y):
         if self.__check_coord(x, y):
-            self.__x = x
-            self.__y = y
+            self.x = x
+            self.y = y
         else:
             print("Координатами могут быть только целые числа")
+    def get_perimetre(self):
+        raise NotImplementedError("В дочернем классе должен быть определен метод get_perimetre()")
 
     @classmethod
     def __check_coord(cls, *args):
@@ -14,8 +17,9 @@ class Geom:
             return False
         else:
             return True
+
     def get_coords(self):
-            print((self.__x, self.__y))
+        print((self.x, self.y))
 
 
 class React(Geom):
@@ -23,7 +27,11 @@ class React(Geom):
         super().__init__(x, y)
         self.check_fill(fill)
         self.fill = fill
+        self.name = "Квадрат"
         print("Вы создали квадрат")
+
+    def get_perimetre(self):
+        return self.x * self.y
 
     @classmethod
     def check_fill(cls, fill):
@@ -31,6 +39,22 @@ class React(Geom):
             raise TypeError("Наличие заливки пишется буквами")
 
 
+class Cube(Geom):
+    def __init__(self, x):
+        y = x
+        super().__init__(x, y)
+        self.name = "Куб"
+        print("Вы создали куб")
+
+    def get_perimetre(self):
+        return self.x**2
+
+
 a = React(0, 1, "Yes")
 print(a.__dict__)
 a.get_coords()
+figures = (Cube(5), React(2, 3))
+k = 0
+for i in figures:
+    k += 1
+    print(f"Периметр равен фигуры {i.name}:", i.get_perimetre())
